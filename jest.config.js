09737f3a0327b63455e35ecd77873ae9f3475e08
Nevitones <neviton@ciandt.com>
2022-09-16 12:31:53 -0300

    Configure Jest and create a dummy test

---
+++ jest.config.js
@@ -0,0 +1,33 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.ts',
+  ],
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/__test__/',
+    '.d.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: [
+    '**/__test__/*.test.ts',
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '.aws-sam/',
+  ],
+  verbose: true,
+
+};
---
+++ src/lambdas/getVehicleLambda/__test__/app.test.ts
@@ -0,0 +1,5 @@
+describe('Foo', () => {
+  it('Bar', () => {
+    expect(1).toEqual(1);
+  });
+});
