941f6a2c054eb0333e0314a56cb7d5efb9f16769
Joan De La Rosa <joanvaldez@ciandt.com>
2022-10-21 10:54:01 -0400

    [cabulosos-ts] fisrt test, happy path

--- src/lambdas/getVehicleLambda/tests/getVehicleLambda.test.ts
+++ src/lambdas/getVehicleLambda/tests/getVehicleLambda.test.ts
@@ -1,3 +1,43 @@
-test('primeiro teste', () => {
-  expect(1).toBe(1);
-});
+import { AppSyncResolverEvent } from 'aws-lambda';
+import { QueryGetVehicleArgs, Vehicle } from 'types/appsync';
+import { handler } from '../app'
+
+describe('Testing handler Jets', () => {
+  test('primeiro teste', async () => {
+    //AWS type 
+    const event: AppSyncResolverEvent<QueryGetVehicleArgs, Vehicle> = {
+      arguments: {
+        id: '1',
+      },
+      source: {
+        __typename: undefined,
+        available: false,
+        category: '',
+        id: '',
+        publishedAt: 0,
+        seats: 0,
+      },
+      request: {
+        headers: {},
+      },
+      info: {
+        selectionSetList: [],
+        selectionSetGraphQL: '',
+        parentTypeName: '',
+        fieldName: '',
+        variables: {},
+      },
+      prev: null,
+      stash: {},
+    };
+
+    const resutl = await handler(event);
+    expect(resutl).toEqual({
+      id: '1',
+      category: 'car',
+      seats: 5,
+      publishedAt: 1658468453,
+      available: true,
+    });
+  });
+})
